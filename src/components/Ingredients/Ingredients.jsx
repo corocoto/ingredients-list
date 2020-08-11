0c6a5ebf84f9742ea8452cd7860032f314239ab9
Artem Gusev <gusev2014russia@mail.ru>
2020-08-11 21:36:27 +0300

    adds functionality for adding ingredients into firebase database

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -7,14 +7,16 @@ import Search from './Search';
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
 
-    const addIngredientHandler = ingredient => {
-        setUserIngredients(prevState => [
-            ...prevState,
-            {
-                id: Math.floor(Math.random() * 10000).toString(),
-                ...ingredient
+    const addIngredientHandler = async ingredient => {
+        const response = await fetch('https://ingredients-list-941ab.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {
+                'Content-type': 'application/json'
             }
-        ]);
+        });
+        const id = response.json().name + Math.floor(Math.random() * 1000).toString();
+        setUserIngredients(prevState => [...prevState, {id, ...ingredient}]);
     };
 
     const removeIngredientHandler = id => {
