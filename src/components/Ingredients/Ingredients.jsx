098ab78b7190b27940a4caaa90d99973e529b9d9
Artem Gusev <gusev2014russia@mail.ru>
2020-08-12 20:49:31 +0300

    adds useEffect hook for getting and loading information from database

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,21 @@ import Search from './Search';
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
+
+    useEffect(() => {
+        fetch('https://ingredients-list-941ab.firebaseio.com/ingredients.json')
+            .then(response => response.json())
+            .then(data => {
+                const loadedIngredients = [];
+                for (const key in data){
+                    loadedIngredients.push({
+                        id: key,
+                        ...data[key]
+                    });
+                }
+                setUserIngredients(loadedIngredients);
+            });
+    }, []); //if second argument's value looks like `[]`, so it'll work like `componentDidMount`
 
     const addIngredientHandler = async ingredient => {
         const response = await fetch('https://ingredients-list-941ab.firebaseio.com/ingredients.json', {
