d7002a253d4dfef94e1e9aa063466018e3b220c5
Artem Gusev <gusev2014russia@mail.ru>
2020-08-15 15:37:41 +0300

    add spinner when loading response from fetch. and modal with error message if we're get an error

--- src/components/Ingredients/IngredientList.jsx
+++ src/components/Ingredients/IngredientList.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
 import './IngredientList.css';
+import LoadingIndicator from '../UI/LoadingIndicator';
 
-const IngredientList = props => {
-  return (
+const IngredientList = props => (
     <section className="ingredient-list">
-      <h2>Loaded Ingredients</h2>
-      <ul>
-        {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
-            <span>{ig.title}</span>
-            <span>{ig.amount}x</span>
-          </li>
-        ))}
-      </ul>
+        <h2>Loaded Ingredients</h2>
+        {props.loading
+            ? <LoadingIndicator/>
+            : <ul>
+                {props.ingredients.map(ig => (
+                    <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+                        <span>{ig.title}</span>
+                        <span>{ig.amount}x</span>
+                    </li>
+                ))}
+            </ul>
+        }
     </section>
-  );
-};
+);
 
 export default IngredientList;
--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -2,37 +2,61 @@ import React, {useState, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     const addIngredientHandler = async ingredient => {
-        const response = await fetch('https://ingredients-list-941ab.firebaseio.com/ingredients.json', {
-            method: 'POST',
-            body: JSON.stringify(ingredient),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        });
-        const id = response.json().name + Math.floor(Math.random() * 1000).toString();
-        setUserIngredients(prevState => [...prevState, {id, ...ingredient}]);
+        setIsLoading(true);
+        try {
+            const response = await fetch('https://ingredients-list-941ab.firebaseio.com/ingredients.json', {
+                method: 'POST',
+                body: JSON.stringify(ingredient),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            });
+            const id = response.json().name + Math.floor(Math.random() * 1000).toString();
+            setUserIngredients(prevState => [...prevState, {id, ...ingredient}]);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+
     };
 
     const removeIngredientHandler = async id => {
-        await fetch(`https://ingredients-list-941ab.firebaseio.com/ingredients/${id}.json`, { method: 'DELETE' });
-        setUserIngredients(prevState => prevState.filter(ingredient => ingredient.id !== id));
+        setIsLoading(true);
+        try {
+            await fetch(`https://ingredients-list-941ab.firebaseio.com/ingredients/${id}.json`, {method: 'DELETE'});
+            setUserIngredients(prevState => prevState.filter(ingredient => ingredient.id !== id));
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
     const filteredIngredientsHandler = useCallback(filteredIngredients => setUserIngredients(filteredIngredients), []);
 
+    const clearError = () => setError('');
+
     return (
         <div className="App">
+            {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
             <IngredientForm onAddIngredient={addIngredientHandler}/>
-
             <section>
                 <Search onLoadIngredients={filteredIngredientsHandler}/>
-                <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+                <IngredientList
+                    ingredients={userIngredients}
+                    onRemoveItem={removeIngredientHandler}
+                    loading={isLoading}
+                />
             </section>
         </div>
     );
